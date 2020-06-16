1835dc6d2bed4fd19b7e5ee103c6675712ed2077
Nerdnickk <56470910+Nerdnickk@users.noreply.github.com>
2020-06-16 14:33:43 +0900

    seconmd

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, createRef } from "react";
+import React from "react";
 import styled, { ThemeProvider } from "styled-components";
 import Theme from "./Styles/Theme";
 import GlobalStyles from "./Styles/GlobalStyles";
